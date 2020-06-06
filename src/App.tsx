062a8f9042be10f5624083c185ead08c467cb176
Mário Fernandes <mariobmf02@gmail.com>
2020-06-06 19:09:00 -0300

    Added all the features of the Home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
 
+import Routes from './routes';
+
 const App: React.FC = () => {
   return (
     <div>
-      <h1>Initial Commit</h1>
+      <Routes />
     </div>
   );
 };
